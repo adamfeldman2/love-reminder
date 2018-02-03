aaec335d1bbc0536b92ed0a0310d7cba97acb6b3
Adam <feldman238@gmail.com>
2018-02-02 20:43:11 -0500

    Update state as inputs are filled

--- client/src/components/ReminderInput.js
+++ client/src/components/ReminderInput.js
@@ -3,19 +3,13 @@ import TextField from 'material-ui/TextField';
 
 const ReminderInput = (props) => {
   const inputStyle = {
-    width: '90%',
-    margin: '0 auto',
+    width: '85%',
+    margin: '5px auto 0',
     display: 'block',
-    fontSize: '1.3em'
+    fontSize: '1.1em'
   };
 
-  return (
-    <TextField
-      hintText="Tell them what you love about them. Keep it short and sweet!"
-      floatingLabelText={props.placeholder}
-      style={inputStyle}
-    />
-  );
+  return <TextField hintText="What do you love about them? Keep it short and sweet!" floatingLabelText={props.placeholder} style={inputStyle} onChange={props.handleInputChange} />;
 };
 
 export default ReminderInput;
