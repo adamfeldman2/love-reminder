df82ba7f9e23a729000c280947165eeec04e8eba
Adam <feldman238@gmail.com>
2018-02-12 07:33:23 -0500

    Update email schedule to send once at 12pm EST

--- jobs/email.js
+++ jobs/email.js
@@ -2,48 +2,48 @@ module.exports = (schedule, mailgun, mongoose, moment) => {
   const User = mongoose.model('users');
 
   // set job's schedule
-  // const job = schedule.scheduleJob('* 14 * * *', function() {
-  //   // today's date
-  //   const today = moment().format('MMM D, YYYY');
+  const job = schedule.scheduleJob('0 17 * * *', function() {
+    // today's date
+    const today = moment().format('MMM D, YYYY');
 
-  //   // find all users in the database
-  //   User.find({}).then((user) => {
-  //     // iterate over all users
-  //     for (let i = 0; i < user.length; i++) {
-  //       // iterate over all reminders
-  //       for (let j = 0; j < 365; j++) {
-  //         // if today's day matches the sendDate AND today's reminder has text
-  //         if (
-  //           user[i].reminders[j].sendDate === today &&
-  //           user[i].reminders[j].text
-  //         ) {
-  //           // build email's html
-  //           const output = `
-  //             <h4>New 365 Reasons Why message from ${user[i].givenName}!</h4>
-  //             <p>${user[i].reminders[j].text}</p>
-  //           `;
+    // find all users in the database
+    User.find({}).then((user) => {
+      // iterate over all users
+      for (let i = 0; i < user.length; i++) {
+        // iterate over all reminders
+        for (let j = 0; j < 365; j++) {
+          // if today's day matches the sendDate AND today's reminder has text
+          if (
+            user[i].reminders[j].sendDate === today &&
+            user[i].reminders[j].text
+          ) {
+            // build email's html
+            const output = `
+              <h4>New 365 Reasons Why message from ${user[i].givenName}!</h4>
+              <p>${user[i].reminders[j].text}</p>
+            `;
 
-  //           // build email info
-  //           var data = {
-  //             from: `${user[i].givenName} <${user[i].email}>`,
-  //             to: user[i].recipient,
-  //             subject: 'New Message From 365 Reasons Why!',
-  //             text: `New 365 Reasons Why message from ${user[i].givenName}: ${
-  //               user[i].reminders[j].text
-  //             }`,
-  //             html: output
-  //           };
-  //           //send message via mailgun
-  //           mailgun.messages().send(data, function(err, body) {
-  //             if (err) {
-  //               console.log('ERROR: ', err);
-  //             } else {
-  //               console.log('BODY: ', body);
-  //             }
-  //           });
-  //         }
-  //       }
-  //     }
-  //   });
-  // });
+            // build email info
+            var data = {
+              from: `${user[i].givenName} <${user[i].email}>`,
+              to: user[i].recipient,
+              subject: 'New Message From 365 Reasons Why!',
+              text: `New 365 Reasons Why message from ${user[i].givenName}: ${
+                user[i].reminders[j].text
+              }`,
+              html: output
+            };
+            //send message via mailgun
+            mailgun.messages().send(data, function(err, body) {
+              if (err) {
+                console.log('ERROR: ', err);
+              } else {
+                console.log('BODY: ', body);
+              }
+            });
+          }
+        }
+      }
+    });
+  });
 };
